fcfb95de3c3ba2a161ff2ae0c5a80a100f0cdd45
Jon <jstapleton463@harrisonburg.k12.va.us>
2020-02-10 10:42:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-# Before running this program, you must make sure you have installed pygame
-# In the terminal, run the following command:
-# py -m pip install pygame --user
-
-import pygame # import the game engine
-pygame.init() # initialize the engine
+import pygame # import library
+pygame.init()
 
 # Create the window
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((800, 600))
 
-# Set up a loop
 run = True
 while run:
-  
-  # Game code starts here ---------------------------------------
-    
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+
+# Game code starts here ---------------------
+  win.fill((0, 0, 0))
+
   # Draw a rectangle
-  pygame.draw.rect(win, (255, 0, 0), (50, 50, 50, 50))
+  pygame.draw.rect(win, (0, 204, 102), (50, 50, 100, 200))
   
   #Update the display
   pygame.display.update()
+
+print("Ending game")
 pygame.quit()
