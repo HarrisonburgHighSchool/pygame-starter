756021d66028e7e9f745a9c46516eb1695f96f7d
Jon <jstapleton463@harrisonburg.k12.va.us>
2020-03-02 10:39:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
 import pygame
 pygame.init()
 
-def drawEnemy(dict):
-    win.blit(dict["img"], (dict["x"], dict["y"]))
-
-def makeEnemy():
-    enemy_img = pygame.Surface([16, 16]).convert()
-    enemy_img.blit(img, (0, 0), (16, 48, 16, 16))
-    e = {
-        "x": 400,
-        "y": 300,
-        "img": pygame.transform.scale(enemy_img, (64, 64))
-    }
-    return e
-
 
 # Create the clock object
 clock = pygame.time.Clock()
 
 win = pygame.display.set_mode((800, 600))
-img = pygame.image.load('assets/hero/Old hero.png')
+img = pygame.image.load('assets/gfx/Inner.png')
 smol_img = pygame.Surface([16, 16]).convert()
-smol_img.blit(img, (0, 0), (16, 0, 16, 16))
+smol_img.blit(img, (0, 0), (0, 0, 16, 16))
 smol_img = pygame.transform.scale(smol_img, (64, 64))
 
-# render text
-
-
-
-x = 200
-y = 200 
-
-enemy = makeEnemy()
-enemy2 = makeEnemy()
 
 
 run = True
@@ -40,41 +18,14 @@ while run:
     # Limit the framerate to ~120 fps
     clock.tick(120)
 
-    # Player stuff
-    keys=pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= 3
-    if keys[pygame.K_RIGHT]:
-        x += 3
-
-    # Enemy stuff
-
-    if x < enemy["x"]:
-        enemy["x"] -= 1
-
-    if x > enemy["x"]:
-        enemy["x"] += 1
-
-
-
-
-
-
-
-
-
-
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     win.fill((0, 0, 0))
-    win.blit(smol_img, (x, y))
-
-    drawEnemy(enemy)
-    drawEnemy(enemy2)
+    win.blit(smol_img, (0, 0))
+    win.blit(smol_img, (40, 0))
 
 
     pygame.display.update()
